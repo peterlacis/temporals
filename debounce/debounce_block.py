daf1085bb088cbf21b733076a1c9b12495fb307e
Oren Leiman <oleiman@neutral.io>
2014-07-08 14:47:47 -0700

    BLK-39 Adds __init__.py to temporals and fixes bugs in Debouncer
    Signed-off-by: Oren Leiman <oleiman@neutral.io>

--- debounce/debounce_block.py
+++ debounce/debounce_block.py
@@ -1,17 +1,20 @@
+from datetime import datetime
 from nio.common.block.base import Block
 from nio.common.discovery import Discoverable, DiscoverableType
 from nio.metadata.properties.timedelta import TimeDeltaProperty
 
 
 @Discoverable(DiscoverableType.block)
-class Debouncer(Block, GroupBlock):
+class Debouncer(Block):
 
     interval = TimeDeltaProperty()
 
     def __init__(self):
+        super().__init__()
         self._last_emission = None
 
     def configure(self, context):
+        super().configure(context)
         self._last_emission = self.persistence.load('last_emission')
     
     def process_signals(self, signals):
@@ -19,7 +22,9 @@ class Debouncer(Block, GroupBlock):
         if self._last_emission is None or \
            now - self._last_emission > self.interval:
             self._logger.debug("Emitting a signal at %s" % now)
+            self._last_emission = now
+            self.persistence.store('last_emission', now)
+            self.persistence.save()
             self.notify_signals(signals[:1])
-            self.persistence.save('last_emission', now)
-            self.persistence.store()
+
         
