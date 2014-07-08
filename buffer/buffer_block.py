3411935b192670dc05d5811614da8689d94338ab
Oren Leiman <oleiman@neutral.io>
2014-07-08 15:16:19 -0700

    BLK-40 BLK-39 Fixes debouncer and implements buffer
    Signed-off-by: Oren Leiman <oleiman@neutral.io>

--- buffer/buffer_block.py
+++ buffer/buffer_block.py
@@ -5,32 +5,41 @@ from nio.metadata.properties.timedelta import TimeDeltaProperty
 
 
 @Discoverable(DiscoverableType.block)
-class Debouncer(Block):
+class Buffer(Block):
 
     interval = TimeDeltaProperty()
 
     def __init__(self):
         super().__init__()
         self._last_emission = None
+        self._cache = None
 
     def configure(self, context):
         super().configure(context)
         self._last_emission = self.persistence.load('last_emission')
+        self._cache = self.persistence.load('cache') or []
 
     def stop(self):
         self._backup()
     
     def process_signals(self, signals):
         now = datetime.utcnow()
+        self._cache.extend(signals)
         if self._last_emission is None or \
            now - self._last_emission > self.interval:
-            self._logger.debug("Emitting a signal at %s" % now)
+            self._logger.debug(
+                "Emitting %d signals at %s" % (len(self._cache), now))
+                
             self._last_emission = now
-            self.notify_signals(signals[:1])
+            self.notify_signals(self._cache)
+            
+            # reset the cache and persist state data
+            self._cache = []
             self.persistence.store('last_emission', now)
             self._backup()
 
     def _backup(self):
+        self.persistence.store('cache', self._cache)
         self.persistence.save()
         
 
