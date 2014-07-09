4dc9da1addb95a6be11ec144075e096c392ba7fd
Oren Leiman <oleiman@neutral.io>
2014-07-09 15:56:54 -0700

    Moves persistence logic from GroupBy to Debouncer
    Signed-off-by: Oren Leiman <oleiman@neutral.io>

--- debounce/debounce_block.py
+++ debounce/debounce_block.py
@@ -8,23 +8,32 @@ from blocks.base_blocks.group_by.group_by_block import GroupBy
 @Discoverable(DiscoverableType.block)
 class Debouncer(Block, GroupBy):
 
+    """ A block to ensure that only one signal from each of the configured
+    groups will be notified over the configured interval
+
+    Properties:
+        group_by (expression): The value by which signals are grouped.
+        interval (timedelta): At most one signal from each group will 
+            be emitted per this interval.
+
+    """
+
     interval = TimeDeltaProperty()
 
     def __init__(self):
         Block.__init__(self)
         GroupBy.__init__(self)
         self._last_emission = {}
+        self._dirty = False
 
     def configure(self, context):
         super().configure(context)
         self._last_emission = self.persistence.load('last_emission') or {}
-
-    def stop(self):
-        self._backup()
+        self._groups = self.persistence.load('groups') or []
     
     def process_signals(self, signals):
-        self.for_each_group(signals, self.process_group)
-        self.persist_if_dirty()
+        self.for_each_group(self.process_group, signals)
+        self._persist_if_dirty()
 
     def process_group(self, signals, key):
         now = datetime.utcnow()
@@ -36,8 +45,15 @@ class Debouncer(Block, GroupBy):
             self._dirty = True
             self.notify_signals(signals[:1])
 
+    def _persist_if_dirty(self):
+        if self._dirty:
+            self._store()
+            self._backup()
+            self._dirty = False
+
     def _store(self):
         self.persistence.store('last_emission', self._last_emission)
+        self.persistence.store('groups', self._groups)
 
     def _backup(self):
         self.persistence.save()
