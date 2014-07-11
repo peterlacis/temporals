8d7fa5932aa44947ab14c92a9232179dd4e74f75
Chris Brackert <cbrackert@gmail.com>
2014-07-10 17:36:11 -0700

    adds timeout option and persistance as an option

--- buffer/buffer_block.py
+++ buffer/buffer_block.py
@@ -1,14 +1,20 @@
 from datetime import datetime
 from nio.common.block.base import Block
 from nio.common.discovery import Discoverable, DiscoverableType
+from nio.metadata.properties.bool import BoolProperty
 from nio.metadata.properties.timedelta import TimeDeltaProperty
+from nio.metadata.properties.string import StringProperty
 from nio.modules.scheduler.imports import Job
+from nio.common.signal.base import Signal
 
 
 @Discoverable(DiscoverableType.block)
 class Buffer(Block):
 
     interval = TimeDeltaProperty()
+    timeout = BoolProperty(default=False)
+    timeout_attr = StringProperty(visible=False, default="timeout")
+    use_persistence = BoolProperty(visible=False, default=False)
 
     def __init__(self):
         super().__init__()
@@ -18,8 +24,9 @@ class Buffer(Block):
 
     def configure(self, context):
         super().configure(context)
-        self._last_emission = self.persistence.load('last_emission')
-        self._cache = self.persistence.load('cache') or []
+        if self.use_persistence:
+            self._last_emission = self.persistence.load('last_emission')
+            self._cache = self.persistence.load('cache') or []
 
     def start(self):
         now = datetime.utcnow()
@@ -33,7 +40,8 @@ class Buffer(Block):
         )
 
     def stop(self):
-        self._backup()
+        if self.use_persistence:
+            self._backup()
 
     def emit(self, reset=False):
         if reset:
@@ -44,18 +52,19 @@ class Buffer(Block):
                 True
             )
         self._last_emission = datetime.utcnow()
-        self.persistence.store('last_emission', self._last_emission)
         signals = self._cache
         self._cache = []
-        self.notify_signals(signals)
-        self._backup()
-    
+        if signals:
+            self.notify_signals(signals)
+        elif self.timeout:
+            self.notify_signals([Signal({self.timeout_attr: True})])
+        if self.use_persistence:
+            self.persistence.store('last_emission', self._last_emission)
+            self._backup()
+
     def process_signals(self, signals):
         self._cache.extend(signals)
-        
+
     def _backup(self):
         self.persistence.store('cache', self._cache)
         self.persistence.save()
-        
-
-        
